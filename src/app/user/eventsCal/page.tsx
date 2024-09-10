3725aa94b6fe91da8658aca18792a8f23062993a
SantiagoAlegre <alegresantisp@gmail.com>
2024-09-09 22:23:09 -0300

    calendario y ordenes completo

---
+++ src/app/user/eventsCal/page.tsx
@@ -0,0 +1,13 @@
+import EventsCalendar from '@/components/SideBar/EventsCalendar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-black min-h-screen text-white p-4">
+         <EventsCalendar/>
+    </div>
+   
+  )
+}
+
+export default page
