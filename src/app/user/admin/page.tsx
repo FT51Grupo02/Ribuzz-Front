5f66b7aa89f6750edfc2aa9effb7a93006e4ea77
SantiagoAlegre <alegresantisp@gmail.com>
2024-09-11 23:31:17 -0300

    dashboardDeAdministradorCasiTerminada

---
+++ src/app/user/admin/page.tsx
@@ -0,0 +1,13 @@
+import VariosComponentesAdmin from '@/components/Admin/VariosComponentesAdmin'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <VariosComponentesAdmin/>
+      
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/user/admin/usersedit/page.tsx
@@ -0,0 +1,12 @@
+import GestionDeUsuarios from '@/components/Admin/GestionDeUsuarios'
+import React from 'react'
+
+const UsersEdit = () => {
+  return (
+    <div>
+      <GestionDeUsuarios/>
+    </div>
+  )
+}
+
+export default UsersEdit
